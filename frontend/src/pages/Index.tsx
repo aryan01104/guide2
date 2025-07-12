1d8a312e37cb144daa96c019c86962124b665bf6
Aryan Agarwal <aaa344@cornell.edu>
2025-07-12 23:12:14 +0530

    Import Vite/React frontend from session-score-board into frontend/

---
+++ frontend/src/pages/Index.tsx
@@ -0,0 +1,11 @@
+import { SessionCalendar } from "@/components/SessionCalendar";
+
+const Index = () => {
+  return (
+    <div className="min-h-screen bg-background">
+      <SessionCalendar />
+    </div>
+  );
+};
+
+export default Index;
